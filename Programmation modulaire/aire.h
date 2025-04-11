c1f2fd44aee5ee5ef6ac055bcbfa94541ad4fd8e
Senarioes <121971231+Senarioes@users.noreply.github.com>
2025-04-11 22:20:38 +0200

    Initiez-vous à la programmation modulaire

---
+++ Programmation modulaire/aire.h
@@ -0,0 +1,7 @@
+#ifndef aire_h
+#define aire_h
+#include <stdio.h>
+
+double aireRectangle(double largeur, double hauteur);
+
+#endif /*aire_h*/
---
+++ Programmation modulaire/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "aire.h"
+
+int main(int argc, const char * argv[]) {
+
+    double resultat;
+    resultat = aireRectangle(10.0, 20.00);
+
+    printf("L'aire du rectangle est %f\n", resultat);
+    return 0;
+}
