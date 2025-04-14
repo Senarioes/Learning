277198ae31940d7fc18ea01741b4e7c875fdad6c
Senarioes <121971231+Senarioes@users.noreply.github.com>
2025-04-14 21:00:53 +0200

    Créez des variables de type "tableaux"

---
+++ Variable type tableaux/tableau2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+
+    int tableau[]= {10, 23, 50, 100}, i = 0;
+
+    
+    for (i = 0 ; i < 4 ; i++)
+    {
+        printf("%d\n", tableau[i]);
+    }
+
+    return 0;
+}
---
+++ Variable type tableaux/tailletableau.h
@@ -0,0 +1,8 @@
+#ifndef tailletableau_h
+#define tailletableau_h
+#include <stdio.h>
+
+// Prototype de la fonction d'affichage
+void affiche(int *tableau, int tailleTableau);
+
+#endif /*tailletableau_h*/
